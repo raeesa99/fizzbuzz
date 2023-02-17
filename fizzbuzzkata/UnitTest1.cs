1318237216390086f2a7617802fbb44c7c379076
raeesa99 <raeesapatel99@hotmail.com>
2023-02-17 11:44:49 +0000

    test passed for number divisible by 5

--- fizzbuzzkata/UnitTest1.cs
+++ fizzbuzzkata/UnitTest1.cs
@@ -13,11 +13,21 @@ namespace fizzbuzzkata
             var result = check(3);
             Assert.Equal("Fizz", result);
         }
+
+        [Fact]
+        public void IntIsDivisibleBy5()
+        {
+            var result = check(5);
+            Assert.Equal("Buzz", result);
+        }
         public string check(int number)
         {
             if (number % 3 == 0)
             {
                 return "Fizz";
+            } else if (number % 5 == 0)
+            {
+                return "Buzz";
             }
             else
             {
