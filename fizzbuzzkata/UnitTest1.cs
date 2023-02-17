82662b19a28eec7185001453ed12eca560819a17
raeesa99 <raeesapatel99@hotmail.com>
2023-02-17 11:41:34 +0000

    test passed for number divisible by 3

--- fizzbuzzkata/UnitTest1.cs
+++ fizzbuzzkata/UnitTest1.cs
@@ -8,9 +8,21 @@ namespace fizzbuzzkata
             var result = check(1);
             Assert.Equal("1", result);
         }
-        public string check(int number) {
-            return "1";
-
+        [Fact]
+        public void IntIsDivisibleBy3() {
+            var result = check(3);
+            Assert.Equal("Fizz", result);
+        }
+        public string check(int number)
+        {
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return number.ToString();
+            }
         }
     }
 }
